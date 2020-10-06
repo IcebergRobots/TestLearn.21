b8e6f06c156523949cccdf27bafda0668f37396c
Ben <ben.blankenburg@icloud.com>
2020-10-06 16:11:46 +0200

    +added macros

--- TestLearn.21.ino
+++ TestLearn.21.ino
@@ -1,38 +1,70 @@
+#include "Arduino.h"
+
+#define PIN_MOTOR1_DIR_A 38
+#define PIN_MOTOR1_PWR 12
+#define PIN_MOTOR1_DIR_B 40
 
 void setup()
-{
-    pinMode(38, OUTPUT);
-    pinMode(40, OUTPUT);
-    pinMode(12, OUTPUT);
-
+{ 
+    pinMode(PIN_MOTOR1_DIR_A, OUTPUT);
+    pinMode(PIN_MOTOR1_DIR_B,OUTPUT);
+    pinMode(PIN_MOTOR1_PWR,OUTPUT);
 }
 
 void loop()
 {
-     for (int i = 255; i > -255; i--)
-        {
-            motorAn(i);
-            delay(10);
-        }
+    for (int i = -255; i < 255; i++)
+    {
+        motorAn(i);
+        delay(10);
+    }
+    for (int i = 255; i > -255; i--)
+    {
+        motorAn(i);
+        delay(10);
+    }
+   
+// void loop()
+// {
+//     digitalWrite (pinDirA1,HIGH);
+//     digitalWrite (40,LOW);
 
+//     for (int a = 0; a < 256; a++)
+//     {
+//         analogWrite(12,a);
+//         delay(20);
+//     }
     
+//     for (int i = 255; i >= 0; i--)
+//     {
+//         analogWrite(12,i);
+//         delay(20);
+//     }
 }
+
 void motorAn(int power)
 {
-    if (power < 0)
+    if (power<0)
     {
-        digitalWrite(38,LOW);
-        digitalWrite(40,HIGH);
-        power= -power;
-       
-    
+        digitalWrite (PIN_MOTOR1_DIR_A, LOW);
+        digitalWrite (PIN_MOTOR1_DIR_B,HIGH); 
+
+        power = -power;
     }
     else
     {
-        digitalWrite(38,HIGH);
-        digitalWrite(40,LOW);
+        digitalWrite (PIN_MOTOR1_DIR_A, HIGH);
+        digitalWrite (PIN_MOTOR1_DIR_B,LOW); 
+
+     
     }
- 
-    analogWrite(12,power);
     
+    
+    analogWrite (PIN_MOTOR1_PWR,power);  
+}   
+void motorAus()
+{
+    digitalWrite (PIN_MOTOR1_DIR_A, LOW);
+    digitalWrite (PIN_MOTOR1_DIR_B,LOW);  
+    digitalWrite (PIN_MOTOR1_PWR,LOW);  
 }
